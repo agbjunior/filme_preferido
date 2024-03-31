c6972df09e3e46931cfc0b8f12f63d0fa05c1ab3
agbjunior <adilsonguimaraes1@hotmail.com>
2024-03-31 15:23:01 +0100

    config final

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify')
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'))
+}
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+
 }
 
 function imagens(){
@@ -14,8 +22,9 @@ function imagens(){
         .pipe(gulp.dest('./dist/imagens'))
 }
 
-exports.default = gulp.parallel(styles, imagens)
+exports.default = gulp.parallel(styles, imagens, scripts)
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('.src/scripts/*.js', gulp.parallel(scripts))
 }
